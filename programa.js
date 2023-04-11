36ec3dea99ed5a4c92d88c36f8b1c0a5a9117b5f
Emilychaparro <ebohorquez526@gmail.com>
2023-04-10 21:38:08 -0500

    Taller 3

---
+++ programa.js
@@ -0,0 +1,9 @@
+//Crear lo eventlistener
+let botonAlmacenar=document.getElementById(btnAlmacenar);
+botonAlmacenar.addEventListener("click", ()=>{
+    console.log("estoy respondiendo!!!");
+     
+
+
+
+});
